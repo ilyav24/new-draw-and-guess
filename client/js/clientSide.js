0b4c4e36efeb77eabab816a2f2283db6110a8a32
ilyav24 <vogman@gmail.com>
2020-12-11 15:17:53 +0200

    added beginning of player turn

--- client/js/clientSide.js
+++ client/js/clientSide.js
@@ -1,6 +1,6 @@
-websocketGame={
+websocketGame = {
     // constants
-
+    WORD_ANSWER : 0
 }
 
 // canvas context
@@ -8,9 +8,9 @@ websocketGame={
 // var ctx = canvas.getContext('2d');
 
 // initialize script when DOM is ready
-$(function (){
+$(function () {
     // check for existance of WebSockets in browser
-    if (window["WebSocket"]){
+    if (window["WebSocket"]) {
 
         //create connection
         websocketGame.socket = new WebSocket("ws://127.0.0.1:8000");
@@ -21,21 +21,28 @@ $(function (){
         };
 
         // on close event
-        websocketGame.socket.onclose = function(e) {
+        websocketGame.socket.onclose = function (e) {
             console.log("WebSocket is closed now.");
         };
 
         // handle message events
-        websocketGame.socket.onmessage = function(e){
-            console.log("onmessage event: ",e.data);
-            $("#chat-history").append("<li>"  + e.data + "</li>");
+        websocketGame.socket.onmessage = function (e) {
+            console.log("onmessage event: ", e.data);
+            $("#chat-history").append("<li>" + e.data + "</li>");
         }
 
         //get selected word
-        $("#easy-button").on("click",function(){
-            var text=$(this).attr('value');
-            $("#chat-history").append("<li> Please draw the word: "  + text + "</li>");
+        $("#easy-button").on("click", function () {
+            var text = $(this).attr('value');
+            $("#chat-history").append("<li> Please draw the word: " + text + "</li>");
+            var data = {
+                dataType: websocketGame.WORD_ANSWER,
+                message: text
+            }
+
+            console.log("sending: " + text)
+            websocketGame.socket.send(JSON.stringify(data));
         });
-        
+
     }
 })
